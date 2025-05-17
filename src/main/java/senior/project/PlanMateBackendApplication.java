1c9a120e37c7e7cb4cc6754bc7331497eea6ca73
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2025-05-17 11:10:17 +0700

    initial commit

---
+++ src/main/java/senior/project/PlanMateBackendApplication.java
@@ -0,0 +1,12 @@
+package senior.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanMateBackendApplication.class, args);
+	}
+}
